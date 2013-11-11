6ab9fa01415e2b2cae11c362442e0dd94d19919d
Devyn Cairns <devyn.cairns@gmail.com>
2013-11-11 12:43:40 -0800

    [kernel] hello art

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -7,12 +7,25 @@
 #if defined(__linux__)
 #error "You are not using a cross-compiler, you will most certainly run into trouble"
 #endif
- 
+
 #if defined(__cplusplus)
 extern "C" /* Use C linkage for kernel_main. */
 #endif
 void kernel_main()
 {
 	terminal_initialize();
-	terminal_writestring("Hello, kernel World!\nNewline test!");
+  terminal_setcolor(make_color(COLOR_WHITE, COLOR_RED));
+  terminal_clear();
+
+  terminal_setcolor(make_color(COLOR_RED, COLOR_WHITE));
+	terminal_writestring("Kit Version 0.1\n");
+
+  terminal_setcolor(make_color(COLOR_WHITE, COLOR_RED));
+  terminal_writestring("\n* says Hello! *\n\n");
+
+  terminal_writestring("\xdb\x20\x20\x20\xdb \xdb\xdb\xdb\xdb\xdb \xdb\x20\x20\x20\x20 \xdb\x20\x20\x20\x20 \x20\xdb\xdb\xdb\x20\n");
+  terminal_writestring("\xdb\x20\x20\x20\xdb \xdb\x20\x20\x20\x20 \xdb\x20\x20\x20\x20 \xdb\x20\x20\x20\x20 \xdb\x20\x20\x20\xdb\n");
+  terminal_writestring("\xdb\xdb\xdb\xdb\xdb \xdb\xdb\xdb\xdb\xdb \xdb\x20\x20\x20\x20 \xdb\x20\x20\x20\x20 \xdb\x20\x20\x20\xdb\n");
+  terminal_writestring("\xdb\x20\x20\x20\xdb \xdb\x20\x20\x20\x20 \xdb\x20\x20\x20\x20 \xdb\x20\x20\x20\x20 \xdb\x20\x20\x20\xdb\n");
+  terminal_writestring("\xdb\x20\x20\x20\xdb \xdb\xdb\xdb\xdb\xdb \xdb\xdb\xdb\xdb\xdb \xdb\xdb\xdb\xdb\xdb \x20\xdb\xdb\xdb\x20\n");
 }
