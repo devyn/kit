030fd062c46b83320049fce894cd29747cdbbd21
Devyn Cairns <devyn.cairns@gmail.com>
2015-02-04 17:14:06 -0800

    [system] why not false too

---
+++ system/util/false.c
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ *
+ * kit/system/util/false.c
+ * - exits with status 0
+ *
+ * vim:ts=2:sw=2:et:tw=80:ft=c
+ *
+ * Copyright (C) 2013, Devyn Cairns
+ * Redistribution of this file is permitted under the terms of the simplified
+ * BSD license. See LICENSE for more information.
+ *
+ ******************************************************************************/
+
+#define UNUSED __attribute__((__unused__))
+
+int main(UNUSED int argc, UNUSED char **argv)
+{
+  return 1;
+}
