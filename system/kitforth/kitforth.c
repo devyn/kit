7256c0776fe279e5136f24a665c3f9471079162d
Devyn Cairns <devyn.cairns@gmail.com>
2016-01-26 01:37:34 -0800

    compilation support :)

--- system/kitforth/kitforth.c
+++ system/kitforth/kitforth.c
@@ -20,9 +20,14 @@
 #define UNUSED __attribute__((unused))
 
 char line[4096];
-uint64_t data_stack[512];
+char *in = NULL;
 
-uint64_t *dp = data_stack + 512;
+#define DATA_STACK_SIZE 512
+#define DATA_STACK_SAFE 504
+
+uint64_t data_stack[DATA_STACK_SIZE];
+
+uint64_t *dp = data_stack + DATA_STACK_SAFE;
 
 void upcase(char *str) {
   while (*str != '\0') {
@@ -34,7 +39,7 @@ void upcase(char *str) {
 }
 
 void init_dict();
-void interpret();
+void consume_line();
 void printdata();
 
 int main(UNUSED int argc, UNUSED char **argv) {
@@ -44,7 +49,10 @@ int main(UNUSED int argc, UNUSED char **argv) {
     printf("\x1b[1;32mok] \x1b[0;1m");
     char *res = fgets(line, 4096, stdin);
     fputs("\x1b[0m", stdout);
-    if (res != NULL) interpret();
+    if (res != NULL) {
+      in = line;
+      consume_line();
+    }
   }
   return 0;
 }
@@ -53,22 +61,22 @@ int main(UNUSED int argc, UNUSED char **argv) {
 // Returns new data pointer
 extern uint64_t *execute(void (**ip)(), uint64_t *dp);
 
-// NOT CALLABLE! These are asm routines
-extern void push();
-extern void add();
-extern void dup();
-extern void display();
-extern void cr();
-extern void ret();
+#define DICT_TYPE_PRIMITIVE 0x01
+#define DICT_TYPE_CODE      0x02
+#define DICT_TYPE_CONSTANT  0x03
 
-#define DICT_PRIMITIVE 1
-#define DICT_CONSTANT  2
+#define DICT_FLAG_IMMEDIATE 0x01
 
-#define WORD_LENGTH 51
+#define WORD_LENGTH 47
 
 struct dict_entry {
-  int  type; // 4 bytes
+  uint16_t type; // 2 bytes
+  uint16_t flags; // 2 bytes
+  uint16_t len; // 2 bytes
+  uint16_t cap; // 2 bytes
+
   char name[WORD_LENGTH + 1];
+
   union {
     void (*as_code)();
     void *as_ptr;
@@ -80,56 +88,123 @@ struct dict_entry *dict;
 int dict_len = 0;
 int dict_cap = 0;
 
+struct dict_entry *last_word = NULL;
+
 bool append_primitive(const char *name, void (*code)()) {
   if (dict_len < dict_cap) {
-    dict[dict_len].type = DICT_PRIMITIVE;
+    dict[dict_len].type = DICT_TYPE_PRIMITIVE;
+    dict[dict_len].flags = 0;
 
     strncpy(dict[dict_len].name, name, WORD_LENGTH);
     dict[dict_len].name[WORD_LENGTH] = '\0';
     upcase(dict[dict_len].name);
 
-    printf("PRIMITIVE %s = %p.\n", dict[dict_len].name, (void *) code);
-
     dict[dict_len].value.as_code = code;
+
+    last_word = &dict[dict_len];
+
+    printf("PRIMITIVE %s = %p.\n", last_word->name, last_word->value.as_ptr);
+
     dict_len++;
-    return 1;
+    return true;
   }
   else {
     puts("Dictionary is full!");
-    return 0;
+    return false;
+  }
+}
+
+#define CODE_SIZE 256
+
+bool append_code(const char *name) {
+  if (dict_len < dict_cap) {
+    dict[dict_len].type = DICT_TYPE_CODE;
+    dict[dict_len].flags = 0;
+    dict[dict_len].len = 0;
+    dict[dict_len].cap = CODE_SIZE;
+
+    strncpy(dict[dict_len].name, name, WORD_LENGTH);
+    dict[dict_len].name[WORD_LENGTH] = '\0';
+    upcase(dict[dict_len].name);
+
+    dict[dict_len].value.as_ptr = calloc(CODE_SIZE, sizeof(void (*)()));
+
+    last_word = &dict[dict_len];
+
+    printf("CODE      %s = %p.\n", last_word->name, last_word->value.as_ptr);
+
+    dict_len++;
+    return true;
+  }
+  else {
+    puts ("Dictionary is full!");
+    return false;
   }
 }
 
 bool append_constant(const char *name, uint64_t value) {
   if (dict_len < dict_cap) {
-    dict[dict_len].type = DICT_CONSTANT;
+    dict[dict_len].type = DICT_TYPE_CONSTANT;
+    dict[dict_len].flags = 0;
 
     strncpy(dict[dict_len].name, name, WORD_LENGTH);
     dict[dict_len].name[WORD_LENGTH] = '\0';
     upcase(dict[dict_len].name);
 
+    dict[dict_len].value.as_int = value;
+
     printf("CONSTANT  %s = %lx.\n", dict[dict_len].name, value);
 
-    dict[dict_len].value.as_int = value;
-    dict_len++;
+    last_word = &dict[dict_len];
 
-    return 1;
+    dict_len++;
+    return true;
   }
   else {
     puts("Dictionary is full!");
-    return 0;
+    return false;
   }
 }
+
+void immediate() {
+  last_word->flags |= DICT_FLAG_IMMEDIATE;
+}
+
+// NOT CALLABLE! These are asm routines
+extern void push();
+extern void add();
+extern void dup();
+extern void display();
+extern void emit();
+extern void in_char();
+extern void compiler_off();
+extern void compiler_on();
+extern void call();
+extern void ret();
+extern void ret_quit();
+extern void literal_stub();
+extern void postpone_stub();
+extern void immediate_stub();
+extern void defword_stub();
+extern void endword_stub();
 
 void init_dict() {
   dict_cap = 512;
 
   dict = calloc(dict_cap, sizeof(struct dict_entry *));
 
-  append_primitive("+",   &add);
-  append_primitive("dup", &dup);
-  append_primitive(".",   &display);
-  append_primitive("cr",  &cr);
+  append_primitive("+",         &add);
+  append_primitive("dup",       &dup);
+  append_primitive(".",         &display);
+  append_primitive("emit",      &emit);
+  append_primitive("char",      &in_char);
+  append_primitive("[",         &compiler_off); immediate();
+  append_primitive("]",         &compiler_on);
+  append_primitive("literal",   &literal_stub); immediate();
+  append_primitive("postpone",  &postpone_stub); immediate();
+  append_primitive("immediate", &immediate_stub);
+  append_primitive(":",         &defword_stub);
+  append_primitive(";",         &endword_stub); immediate();
 
   append_constant("one", 1);
 }
@@ -144,42 +219,145 @@ struct dict_entry *find_in_dict(char *word) {
   return NULL;
 }
 
-void interpret() {
-  char *in = line;
+bool read_word(char *word) {
+  int i;
 
-  void (*code[3])();
+  for (i = 0;
+       (*in != '\0' &&
+        *in != '\n' &&
+        *in != ' ' &&
+        i < WORD_LENGTH);
+       i++, in++) {
+
+    word[i] = *in;
+  }
+  word[i] = '\0';
+
+  return i != 0;
+}
+
+int read_charword() {
   char word[WORD_LENGTH + 1];
 
-  while (*in != '\n') {
-    int i;
+  while (*in == ' ');
 
-    for (i = 0; *in != '\n' && *in != ' ' && i < WORD_LENGTH; i++, in++) {
-      word[i] = *in;
-    }
-    word[i] = '\0';
+  if (!read_word(word)) {
+    return 0;
+  }
+  else {
+    return word[0];
+  }
+}
+
+int compiling = 0;
+
+void interpret(char *word);
+void compile(char *word);
+
+void consume_line() {
+  char word[WORD_LENGTH + 1];
+
+  while (*in != '\0' && *in != '\n') {
+    while (*in == ' ') in++;
+
+    if (!read_word(word)) continue;
 
     upcase(word);
 
     while (*in == ' ') in++;
 
-    struct dict_entry *match;
-    long number;
-    char *endptr;
-
-    if (strlen(word) == 0) {
-      continue;
+    if (compiling) {
+      compile(word);
     }
-    else if ((match = find_in_dict(word)) != NULL) {
+    else {
+      interpret(word);
+    }
+  }
+}
+
+void interpret(char *word) {
+  void (*code[3])();
+
+  struct dict_entry *match;
+  long number;
+  char *endptr;
+
+  if ((match = find_in_dict(word)) != NULL) {
+    switch (match->type) {
+      case DICT_TYPE_PRIMITIVE:
+        code[0] = match->value.as_code;
+        code[1] = &ret_quit;
+        break;
+
+      case DICT_TYPE_CODE:
+        code[0] = &call;
+        code[1] = match->value.as_code;
+        code[2] = &ret_quit;
+        break;
+
+      case DICT_TYPE_CONSTANT:
+        code[0] = &push;
+        code[1] = match->value.as_code;
+        code[2] = &ret_quit;
+        break;
+
+      default:
+        printf("Error: unknown dictionary entry type %i\n", match->type);
+        return;
+    }
+  }
+  else if (number = strtol(word, &endptr, 16), *endptr == '\0') {
+    // Numeric word
+    code[0] = &push;
+    code[1] = (void (*)()) number;
+    code[2] = &ret_quit;
+  }
+  else {
+    printf("Error: unknown word %s\n", word);
+    return;
+  }
+
+  dp = execute(code, dp);
+
+  if (dp > data_stack + DATA_STACK_SAFE) {
+    puts("Stack underflow.");
+    dp = data_stack + DATA_STACK_SAFE;
+    while (*in != '\0');
+  }
+  else if (dp <= data_stack) {
+    puts("Stack overflow.");
+    dp = data_stack + 1;
+    while (*in != '\0');
+  }
+}
+
+bool disable_immediate = false;
+
+void compile(char *word) {
+  void (**code)() = (void (**)()) last_word->value.as_ptr;
+
+  struct dict_entry *match;
+  long number;
+  char *endptr;
+
+  if ((match = find_in_dict(word)) != NULL) {
+    if (!disable_immediate && match->flags & DICT_FLAG_IMMEDIATE) {
+      interpret(word);
+    }
+    else {
       switch (match->type) {
-        case DICT_PRIMITIVE:
-          code[0] = match->value.as_code;
-          code[1] = &ret;
+        case DICT_TYPE_PRIMITIVE:
+          code[last_word->len++] = match->value.as_code;
           break;
 
-        case DICT_CONSTANT:
-          code[0] = &push;
-          code[1] = match->value.as_code;
-          code[2] = &ret;
+        case DICT_TYPE_CODE:
+          code[last_word->len++] = &call;
+          code[last_word->len++] = match->value.as_code;
+          break;
+
+        case DICT_TYPE_CONSTANT:
+          code[last_word->len++] = &push;
+          code[last_word->len++] = match->value.as_code;
           break;
 
         default:
@@ -187,28 +365,61 @@ void interpret() {
           return;
       }
     }
-    else if (number = strtol(word, &endptr, 16), *endptr == '\0') {
-      // Numeric word
-      code[0] = &push;
-      code[1] = (void (*)()) number;
-      code[2] = &ret;
-    }
-    else {
-      printf("Error: unknown word %s\n", word);
-      return;
-    }
-
-    dp = execute(code, dp);
-
-    if (dp > data_stack + 512) {
-      puts("Stack underflow.");
-      break;
-    }
-    else if (dp < data_stack) {
-      puts("Stack overflow.");
-      break;
-    }
   }
+  else if (number = strtol(word, &endptr, 16), *endptr == '\0') {
+    // Numeric word
+    code[last_word->len++] = &push;
+    code[last_word->len++] = (void (*)()) number;
+  }
+  else {
+    printf("Error: unknown word %s\n", word);
+    return;
+  }
+}
+
+void literal(uint64_t value) {
+  void (**code)() = last_word->value.as_ptr;
+
+  code[last_word->len++] = &push;
+  code[last_word->len++] = (void (*)()) value;
+}
+
+void postpone() {
+  char word[WORD_LENGTH + 1];
+
+  if (!read_word(word)) return;
+
+  upcase(word);
+
+  disable_immediate = true;
+  compile(word);
+  disable_immediate = false;
+}
+
+void defword() {
+  char word[WORD_LENGTH + 1];
+
+  if (!read_word(word)) return;
+
+  append_code(word);
+  compiling = 1;
+}
+
+void dumpptrarray(void **ptr, size_t len) {
+  for (size_t i = 0; i < len; i++) {
+    printf("%p ", ptr[i]);
+  }
+  puts("");
+}
+
+void endword() {
+  void (**code)() = last_word->value.as_ptr;
+
+  code[last_word->len++] = &ret;
+
+  dumpptrarray((void **) code, last_word->len);
+
+  compiling = 0;
 }
 
 void printu64x(uint64_t n) {
@@ -220,7 +431,7 @@ void printdata() {
 
   fputs("\x1b[1;33m", stdout);
 
-  for (x = data_stack + 511; x >= dp; x--) {
+  for (x = data_stack + DATA_STACK_SAFE - 1; x >= dp; x--) {
     printu64x(*x);
   }
 
