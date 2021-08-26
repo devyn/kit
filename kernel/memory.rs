9d10d02574a15d86b57c43a9e562e2059a37ce2e
Devyn Cairns <devyn.cairns@gmail.com>
2021-08-25 22:27:08 -0700

    update copyrights

--- kernel/memory.rs
+++ kernel/memory.rs
@@ -4,7 +4,7 @@
  *
  * vim:ft=rust:ts=4:sw=4:et:tw=80
  *
- * Copyright (C) 2015, Devyn Cairns
+ * Copyright (C) 2015-2021, Devyn Cairns
  * Redistribution of this file is permitted under the terms of the simplified
  * BSD license. See LICENSE for more information.
  *
